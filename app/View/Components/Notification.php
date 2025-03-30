00b3bd32d550cfdd1d35fd0be3f84830f1d5a915
Wilmerson <will.phelipe@gmail.com>
2025-03-30 16:53:27 -0300

    remove comment

--- app/View/Components/Notification.php
+++ app/View/Components/Notification.php
@@ -9,9 +9,7 @@ class Notification extends Component
     public function render()
     {
         $notification = session('success') ?? session('error');
-        // Remover a linha que limpa a sessão
-        // session()->forget(['success', 'error']); // Limpa a sessão após pegar a mensagem
-
+       
         return view('components.notification', [
             'notification' => $notification
         ]);
